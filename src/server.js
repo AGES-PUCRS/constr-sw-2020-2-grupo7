67f363b193df40e6c7befffba22347bc857fd53a
Henrique Kops <henriquekops@localhost.localdomain>
2020-09-28 21:12:43 -0300

    add initial server.js code

--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
-/*
-code goes here
-*/
+const express = require('express')
+const mongoose = require('mongoose')
+const requiredir = require('require-dir')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// mongoose.connect('mongodb://localhost:27017/alunos', { useUnifiedTopology: true, useNewUrlParser: true })
+// requiredir('./src/')
+// app.use('/api', require('./src/routes'))
+
+app.listen(3000, () => {
+    console.log("server up and running on port: 3000");
+})
+
