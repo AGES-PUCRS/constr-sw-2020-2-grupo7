cd8b84ff93dba62418637bae98b2a47a70031ecd
Vinicius Lima <kiddlima@outlook.com>
2020-11-16 20:41:59 -0300

    base front project

---
+++ frontend/src/app/students-list/students-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-students-list',
+  templateUrl: './students-list.component.html',
+  styleUrls: ['./students-list.component.css']
+})
+export class StudentsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onTestButtonClick() {
+  }
+
+}
